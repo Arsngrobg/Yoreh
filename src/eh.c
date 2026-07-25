f989a1d767d14d1d2f48ad24426ba1baf35a585d
James Armstrong <arsngrobg@gmail.com>
2026-07-25 04:31:45 +0100

    test(lua): pushing C functions to the runtime

--- src/eh.c
+++ src/eh.c
@@ -5,6 +5,18 @@
 #include "lualib.h"
 #include "lauxlib.h"
 
+#define EHLUA_RETURNS(n)  (n)
+#define EHLUA_VOID_RETURN EHLUA_RETURNS(0)
+
+// example API functions for the editor
+int32_t eh_editor_setfont(lua_State *L) {
+    const char *font_id = lua_tostring(L, -1);
+    printf("[EH (PHONY)] Set editor font to %s\n", font_id);
+    return EHLUA_VOID_RETURN;
+}
+
+#define EHLUA_INIT "./config/init.lua"
+
 // https://www.lua.org/manual/5.1/manual.html#3
 // https://www.codingwiththomas.com/blog/a-lua-c-api-cheat-sheet
 int32_t main(void) {
@@ -12,11 +24,14 @@ int32_t main(void) {
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
 
-    if (luaL_dofile(L, "test/factorial.lua") == 0) {
-        printf("[C] Executed factorial.lua\n");
+    lua_pushcclosure(L, eh_editor_setfont, 0);
+    lua_setglobal(L, "setfont");
+
+    if (luaL_dofile(L, EHLUA_INIT) == 0) {
+        printf("[C] Executed "EHLUA_INIT"\n");
     } else {
         printf("[C] Error reading script\n");
-        luaL_error(L, "Error: %s\n", lua_tostring(L, -1));
+        luaL_error(L, "Error: %s", lua_tostring(L, -1));
     }
 
     lua_close(L);
