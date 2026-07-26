833d566b39ba9847a5b3d3129e340f8bbe8758d3
James Armstrong <arsngrobg@gmail.com>
2026-07-26 02:36:22 +0100

    feat(api): standard usag of lua tables for namespacing functions

--- config/init.lua
+++ config/init.lua
@@ -1,1 +1,7 @@
-setfont("JetBrains Mono")
+-- this is an example init file for the editor config
+-- not reflective of the final API usage
+
+yoreh.editor.setfont("JetBrains Mono")
+
+yoreh.keybinds.undo("<ctrl> c")
+yoreh.keybinds.redo("<ctrl> z")
--- src/eh.c
+++ src/eh.c
@@ -5,27 +5,61 @@
 #include "lualib.h"
 #include "lauxlib.h"
 
-#define EHLUA_RETURNS(n)  (n)
-#define EHLUA_VOID_RETURN EHLUA_RETURNS(0)
+// some ease-of-use macros
+#define EHLUA_RETURN(n)   return (n)
+#define EHLUA_VOID_RETURN EHLUA_RETURN(0)
+
+#define EHLUA_CFUN(id) ehlua_##id
+#define EHLUA_CDEF(id) static int32_t EHLUA_CFUN(id)(lua_State *L)
 
 // example API functions for the editor
-int32_t eh_editor_setfont(lua_State *L) {
+EHLUA_CDEF(editor_setfont) {
     const char *font_id = lua_tostring(L, -1);
     printf("[EH (PHONY)] Set editor font to %s\n", font_id);
-    return EHLUA_VOID_RETURN;
+    EHLUA_VOID_RETURN;
 }
 
-#define EHLUA_INIT "./config/init.lua"
+EHLUA_CDEF(keybinds_undo) {
+    const char *keybind = lua_tostring(L, -1);
+    printf("[EH (PHONY)] Set undo keybind to \"%s\"\n", keybind);
+    EHLUA_VOID_RETURN;
+}
+
+EHLUA_CDEF(keybinds_redo) {
+    const char *keybind = lua_tostring(L, -1);
+    printf("[EH (PHONY)] Set redo keybind to \"%s\"\n", keybind);
+    EHLUA_VOID_RETURN;
+}
 
 // https://www.lua.org/manual/5.1/manual.html#3
 // https://www.codingwiththomas.com/blog/a-lua-c-api-cheat-sheet
+#define EHLUA_INIT "./config/init.lua"
+
 int32_t main(void) {
     printf("Yoreh-ditor!\n");
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
 
-    lua_pushcclosure(L, eh_editor_setfont, 0);
-    lua_setglobal(L, "setfont");
+    // define table that acts as the namespace for the yoreh API functions
+    lua_newtable(L);
+        // editor functions
+        lua_pushstring(L, "editor");
+        lua_newtable(L);
+            lua_pushstring(L, "setfont");
+            lua_pushcfunction(L, EHLUA_CFUN(editor_setfont));
+            lua_settable(L, -3);
+        lua_settable(L, -3);
+        // keybinds functions
+        lua_pushstring(L, "keybinds");
+        lua_newtable(L);
+            lua_pushstring(L, "undo");
+            lua_pushcfunction(L, EHLUA_CFUN(keybinds_undo));
+            lua_settable(L, -3);
+            lua_pushstring(L, "redo");
+            lua_pushcfunction(L, EHLUA_CFUN(keybinds_redo));
+            lua_settable(L, -3);
+        lua_settable(L, -3);
+    lua_setglobal(L, "yoreh");
 
     if (luaL_dofile(L, EHLUA_INIT) == 0) {
         printf("[C] Executed "EHLUA_INIT"\n");
