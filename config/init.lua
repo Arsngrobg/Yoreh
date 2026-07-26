81d6b313009ed7022002853a300edec876557c90
James Armstrong <arsngrobg@gmail.com>
2026-07-26 02:44:23 +0100

    test(api): keybinds for nonexistent terminal at the moment

--- config/init.lua
+++ config/init.lua
@@ -2,6 +2,8 @@
 -- not reflective of the final API usage
 
 yoreh.editor.setfont("JetBrains Mono")
+yoreh.terminal.setfont("JetBrains Mono")
 
+yoreh.keybinds.showterm("<alt> /")
 yoreh.keybinds.undo("<ctrl> c")
 yoreh.keybinds.redo("<ctrl> z")
