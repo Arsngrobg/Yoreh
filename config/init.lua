d641559d07b28207d49220210e8aa276aa5c8490
James Armstrong <arsngrobg@gmail.com>
2026-07-28 01:40:24 +0100

    fix(test): formatting

--- config/init.lua
+++ config/init.lua
@@ -1,9 +1,9 @@
 -- this is an example init file for the editor config
 -- not reflective of the final API usage
 
-yoreh.editor.setfont("JetBrains Mono")
-yoreh.terminal.setfont("JetBrains Mono")
+yoreh.editor.setfont('JetBrains Mono')
+yoreh.terminal.setfont('JetBrains Mono')
 
-yoreh.keybinds.showterm("<alt> /")
-yoreh.keybinds.undo("<ctrl> c")
-yoreh.keybinds.redo("<ctrl> z")
+yoreh.keybinds.showterm('<alt> /')
+yoreh.keybinds.undo('<ctrl> c')
+yoreh.keybinds.redo('<ctrl> z')
